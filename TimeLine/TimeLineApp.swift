3bfba8f7a4c8873173015fe588349c33c9f38bcc
Syed Raza <syed.raza011@gmail.com>
2023-07-15 19:17:50 -0400

    Initial Commit

---
+++ TimeLine/TimeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLineApp.swift
+//  TimeLine
+//
+//  Created by Syed Raza on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
